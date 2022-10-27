092755b44a7ba6d150be51c1e56101ea1c76cc36
shirguin <shirguin@yandex.ru>
2022-10-27 07:26:17 +0300

    lesson_4

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Header from "./components/Header";
+import Image from "./components/Image";
+import img1 from "./img/1.jpg";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends React.Component {
+  text = "Help text"
+
+  render() {
+    return (
+      <div>
+        <Header title="Шапка сайта"/>
+        <Header title="Шапка сайта!"/>
+        <Header title="Шапка сайта!!!"/>
+        <h1>{this.text}</h1>
+        <input placeholder={this.text} onClick={this.inputClick} onMouseEnter={this.mouseOver}/>
+        <p>{this.text === "Help text" ? "Yes" : "No"}</p>
+        <Image image={img1}/>
+      </div>
+    )
+  }
+
+  inputClick() {console.log("Clicked")}
+  mouseOver() {console.log("MouseOver")}   
 }
 
-export default App;
+export default App
